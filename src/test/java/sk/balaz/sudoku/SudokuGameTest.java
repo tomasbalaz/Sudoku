cfdb85ae2a2d838c9c26c05c2f2c4e59b2d69683
Tomas Balaz <tomas.balaz@asseco-ce.com>
2017-11-14 14:52:33 +0100

    Add SudokuGameTest Junit test

---
+++ src/test/java/sk/balaz/sudoku/SudokuGameTest.java
@@ -0,0 +1,15 @@
+package sk.balaz.sudoku;
+
+import org.junit.Before;
+
+/**
+ * Unit test for simple App.
+ */
+public class SudokuGameTest {
+	
+	private Board board;
+
+	@Before
+	public void setUp() {
+	}
+}

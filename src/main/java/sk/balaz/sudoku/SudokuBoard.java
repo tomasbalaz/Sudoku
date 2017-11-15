4802fb17abc8db35b18a2b9d0bdcd62560de5139
Tomas Balaz <tomas.balaz@asseco-ce.com>
2017-11-15 08:40:55 +0100

    Add default sizes for row and columns

--- src/main/java/sk/balaz/sudoku/SudokuBoard.java
+++ src/main/java/sk/balaz/sudoku/SudokuBoard.java
@@ -4,8 +4,21 @@ import java.util.Arrays;
 
 public class SudokuBoard implements Board {
 
+	private static final int DEFAULT_ROW_SIZE = 9;
+	
+	private static final int DEFAULT_COLUMN_SIZE = 9;
+	
 	/** sudoku game board as 2D field */
 	private int[][] gameBoard;
+	
+	
+	public SudokuBoard(String puzzleString) {
+		
+		char[] charArray = puzzleString.toCharArray();
+		int length = charArray.length;
+		
+		gameBoard = new int[DEFAULT_ROW_SIZE][DEFAULT_COLUMN_SIZE];
+	}
 
 	public SudokuBoard(int[][] gameBoard) {
 		this.gameBoard = gameBoard;
@@ -23,6 +36,15 @@ public class SudokuBoard implements Board {
 	public void printBoard() {
 		for (int[] rows : gameBoard) {
 			System.out.println(Arrays.toString(rows));
+		}
+	}
+	
+	private void checkPuzzleStringLength(String puzzleString) {
+		int boardSize = DEFAULT_ROW_SIZE * DEFAULT_COLUMN_SIZE;
+		
+		char[] charArray = puzzleString.toCharArray();
+		if(charArray.length != boardSize) {
+			//throw new 
 		}
 	}
 }
